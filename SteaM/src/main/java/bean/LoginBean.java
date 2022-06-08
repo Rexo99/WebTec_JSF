4a7c6dc820e01c62f9d9f2ff6da412b90ca90c25
marvinpieper <piepermarvin54@googlemail.com>
2022-06-08 13:05:43 +0200

    delete managebean and put the files in to the bean folder

--- SteaM/src/main/java/bean/LoginBean.java
+++ SteaM/src/main/java/bean/LoginBean.java
@@ -1,4 +1,4 @@
-package managedBeans;
+package bean;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
--- SteaM/src/main/java/bean/SearchBean.java
+++ SteaM/src/main/java/bean/SearchBean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package managedBeans;
+package bean;
 
 import de.hsh.steam.entities.Genre;
 import de.hsh.steam.entities.Rating;

da044d5d5e7fa35b14ad65f07f833104c74a3fc8
marvinpieper <piepermarvin54@googlemail.com>
2022-06-08 13:03:06 +0200

    alles kaputt gemacht nicht ü

--- SteaM/src/main/java/bean/RegisterBean.java
+++ SteaM/src/main/java/bean/RegisterBean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Beans/Bean.java to edit this template
  */
-package managedBeans;
+package bean;
 
 import de.hsh.steam.services.SteamService;
 import java.beans.*;
